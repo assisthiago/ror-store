e4115c1b3c1800a439aa18e46027abe1a17e7c4a
Thiago Assis <thiago.medassis@gmail.com>
2025-02-11 18:50:40 -0300

    Controllers and Actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  root "products#index"
   # get "/products", to: "products#index"
   # get "/products/new", to: "products#new"
   # post "/products", to: "products#create"
