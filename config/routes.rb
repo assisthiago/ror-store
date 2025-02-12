80480843f9ed559d915ed788ffaeba0e9ae86f8c
Thiago Assis <thiago.medassis@gmail.com>
2025-02-12 19:16:05 -0300

    Adding In Stock Notifications

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resources :unsubscribe, only: [ :show ]
   root "products#index"
-  # get "/products", to: "products#index"
-  # get "/products/new", to: "products#new"
-  # post "/products", to: "products#create"
-  # get "/products/:id", to: "products#show"
-  # get "/products/:id/edit", to: "products#edit"
-  # patch "/products/:id", to: "products#update"
-  # put "/products/:id", to: "products#update"
-  # delete "/products/:id", to: "products#destroy"
 end
